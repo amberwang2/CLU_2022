061b7cfa2212aeef9d329d1f2fcb99d4f5c58944
massive goose <massivegoose@protonmail.com>
2022-07-20 12:30:49 -0700

    replace stats.py with notebook

--- stats.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import os, sys, math, pdb, glob, time, fnmatch, datetime
-from astropy.table import Table, vstack, hstack, Column, unique, join
-
-def stop(): pdb.set_trace()
-
-cat=Table.read('CatVisClass_AmberW_20220719.fits')
